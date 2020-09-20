02e98691d8d8b4f190263c6626302fdd9c9b408f
Joao Fernando <joao.amorim@alumni.usp.br>
2020-09-20 10:48:00 -0300

    Installed cors

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BsController} from 'react-icons/bs'
 import { Link } from 'react-router-dom'
-// import data from '../../data/games'
 // CSS
 import './Home.scss'
 
